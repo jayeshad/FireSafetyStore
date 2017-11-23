f2c642cc9fd51ea7e596fb71b2c6cd491c00fe5c
Jayesh Dhananjayan <ad.jayesh@live.in>
2017-11-24 00:44:57 +0530

    Added SQL Db Project

---
+++ FireSafetyStore.Database/dbo/Tables/AspNetUsers.sql
@@ -0,0 +1,27 @@
+﻿CREATE TABLE [dbo].[AspNetUsers] (
+    [Id]                   NVARCHAR (128) NOT NULL,
+    [FirstName]            NVARCHAR (15)  NULL,
+    [LastName]             NVARCHAR (15)  NULL,
+    [Address]              NVARCHAR (150) NULL,
+    [State]                NVARCHAR (15)  NULL,
+    [City]                 NVARCHAR (15)  NULL,
+    [PostalCode]           NVARCHAR (MAX) NULL,
+    [Email]                NVARCHAR (256) NULL,
+    [EmailConfirmed]       BIT            NOT NULL,
+    [PasswordHash]         NVARCHAR (MAX) NULL,
+    [SecurityStamp]        NVARCHAR (MAX) NULL,
+    [PhoneNumber]          NVARCHAR (MAX) NULL,
+    [PhoneNumberConfirmed] BIT            NOT NULL,
+    [TwoFactorEnabled]     BIT            NOT NULL,
+    [LockoutEndDateUtc]    DATETIME       NULL,
+    [LockoutEnabled]       BIT            NOT NULL,
+    [AccessFailedCount]    INT            NOT NULL,
+    [UserName]             NVARCHAR (256) NOT NULL,
+    CONSTRAINT [PK_dbo.AspNetUsers] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [UserNameIndex]
+    ON [dbo].[AspNetUsers]([UserName] ASC);
+
