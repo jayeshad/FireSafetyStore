fc6d51b5c5495fb8adddf3272cbbfb0850bbaede
jayeshad <ad.jayesh@live.in>
2018-01-28 01:42:43 +0530

    AddedReportingLogic

---
+++ FireSafetyStore.Web.Client/Controllers/ReportController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Web.Mvc;
+using FireSafetyStore.Web.Client.Infrastructure.DbContext;
+using FireSafetyStore.Web.Client.Models;
+
+namespace FireSafetyStore.Web.Client.Controllers
+{
+    public class ReportController : Controller
+    {
+        private FiresafeDbContext db = new FiresafeDbContext();
+
+        public ActionResult ReportHome()
+        {
+            return View();
+        }
+
+        public ActionResult ReportTemplate(string ReportName, string ReportDescription, int Width, int Height)
+        {
+            var rptInfo = new ReportInfo
+            {
+                ReportName = ReportName,
+                ReportDescription = ReportDescription,
+                ReportURL = String.Format("../../Reports/ReportTemplate.aspx?ReportName={0}&Height={1}", ReportName, Height),
+                Width = Width,
+                Height = Height
+            };
+
+            return View(rptInfo);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ FireSafetyStore.Web.Client/Reports/ReportTemplate.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FireSafetyStore.Web.Client.Reports
+{
+    public partial class ReportTemplate : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                try
+                {
+                    String reportFolder = System.Configuration.ConfigurationManager.AppSettings["SSRSReportsFolder"].ToString();
+                    
+                    rvSiteMapping.Height = Unit.Pixel(Convert.ToInt32(Request["Height"]) - 58);
+                    rvSiteMapping.ProcessingMode = Microsoft.Reporting.WebForms.ProcessingMode.Remote;
+
+                    rvSiteMapping.ServerReport.ReportServerUrl = new Uri("SSRS URL"); // Add the Reporting Server URL 
+                    rvSiteMapping.ServerReport.ReportPath = String.Format("/{0}/{1}", reportFolder, Request["ReportName"].ToString());
+
+                    rvSiteMapping.ServerReport.Refresh();
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+    }
+}
